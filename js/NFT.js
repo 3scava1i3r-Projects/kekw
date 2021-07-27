55a1e2b44a0eea1830cbe76765a7d1794cc0a4a6
Hritwik Tripathi <Tripathi.hritwik@gmail.com>
2021-07-27 22:54:06 +0530

    new patch

--- js/NFT.js
+++ js/NFT.js
@@ -4,9 +4,26 @@ const test = document.getElementById("test");
 const meme = document.getElementById("meme-image");
 
 
+const contractAddress = "0x248dD7b699042C53fAc61A8f4efA15f413E5477b";
+
+let contract_json;
+fetch("../artifacts/MyNFT.json")
+  .then((res) => res.json())
+  .then((result) => {
+    contract_json = result;
+  });
+
 const Web3Modal = window.Web3Modal.default;
 const WalletConnectProvider = window.WalletConnectProvider.default;
 let selectedACC;
+let metadata_hash = null;
+let nonce_needed = null
+let contract = null;
+let NFTName = null;
+let NFTDescription = null;
+
+
+
 
 const ConnectWallet = async () => {
   const providerOptions = {
@@ -36,65 +53,38 @@ const ConnectWallet = async () => {
   selectedACC = accounts[0];
   acc.innerText = selectedACC;
 
+  const nftContract = new web3.eth.Contract(contract_json.abi, contractAddress);
+  contract = nftContract;
+  console.log(contract)
+  const nonce = await web3.eth.getTransactionCount(
+    selectedACC,
+    "latest"
+  )
+
+  nonce_needed = nonce.toString();
+
   if ((selectedACC != null) | undefined) {
     console.log(selectedACC);
   } else {
     console.log("yo! connect the damn wallet");
   }
-};
+};;
 
 web3btn.addEventListener("click", () => {
   ConnectWallet();
 });
 
-const getNFT = () => {
-  
-}
-
 document.getElementById("nft-file-input").addEventListener("change", async (res) => {
   
-  
-  /* console.log(res.target.files[0]);
-  const ipfs = window.IpfsApi("gateway.ipfs.io"); // Connect to IPFS 
-  const Buffer = window.IpfsApi().Buffer;
-  let buf_blob = await res.target.files[0].arrayBuffer();
-  let buf = Buffer(buf_blob);
-
-  ipfs.files.add(buf, (err, result) => {
-    // Upload buffer to IPFS
-    if (err) {
-      console.error(err);
-      return;
-    }
-    let url = `https://ipfs.io/ipfs/${result[0].hash}`;
-    console.log(`Url --> ${url}`);
-  });
-
-  ipfs.add(buf).then((res) => console.log(res)); */
-
-
-
-  const pinataApiKey = process.env.pinataApiKey;
-  const pinataSecretApiKey = process.env.pinataSecretApiKey 
+  const pinataApiKey = "a770d310d147135d5ec4"; 
+  const pinataSecretApiKey =
+    "076b05a1c38c2910d32a8079e1007d52b8c02264990e0af61fa0e544cd760c78";
   const url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+  const jsonUrl = "https://api.pinata.cloud/pinning/pinJSONToIPFS";
 
   let data = new FormData();
-/*   const ipfs = window.IpfsApi("gateway.ipfs.io"); // Connect to IPFS
-  const Buffer = window.IpfsApi().Buffer;
-  let buf_blob = await res.target.files[0].arrayBuffer();
-  let buf = Buffer(buf_blob);
-  console.log(buf) */
   data.append("file", res.target.files[0]);
-  
 
-  const metadata = JSON.stringify({
-    name: "testname",
-    keyvalues: {
-      exampleKey: "exampleValue",
-    },
-  });
-  data.append("pinataMetadata", metadata);
-  
   axios
     .post(url, data, {
       maxBodyLength: "Infinity", //this is needed to prevent axios from erroring out with large files
@@ -104,17 +94,90 @@ document.getElementById("nft-file-input").addEventListener("change", async (res)
         pinata_secret_api_key: pinataSecretApiKey,
       },
     })
-    .then(function (response) {
+    .then( (response) => {
       //handle response here
       console.log(response.data)
-      console.log(`https://ipfs.io/ipfs/` + response.data.IpfsHash);
+      console.log(`image url = https://ipfs.io/ipfs/` + response.data.IpfsHash);
+
+      const metadata = JSON.stringify({
+        name: NFTName,
+        Info: {
+          description: NFTDescription,
+          image:`https://ipfs.io/ipfs/` + response.data.IpfsHash
+        },
+      });
+
+      axios
+        .post(jsonUrl, metadata, {
+          maxBodyLength: "Infinity", //this is needed to prevent axios from erroring out with large files
+          headers: {
+            "Content-Type":'application/json',
+            pinata_api_key: pinataApiKey,
+            pinata_secret_api_key: pinataSecretApiKey,
+          },
+        })
+        .then((res) =>{
+          console.log(`metadata url = https://ipfs.io/ipfs/${res.data.IpfsHash}`)
+          let gg = `https://ipfs.io/ipfs/${res.data.IpfsHash}`
+          metadata_hash = gg
+          
+          console.log("image uploaded")
+        })
     })
     .catch(function (error) {
-      //handle error here
+      console.log(error)
     });
 
 });
 
 
+document.querySelector(".nes-input").addEventListener("change" , (r) => {
+  console.log(r.target.value)
+  NFTDescription = r.target.value;
+})
 
 
+
+document.querySelector(".nes-textarea").addEventListener("change", (res) => {
+  console.log(res.target.value);
+  NFTName = res.target.value;
+});
+
+  
+document.getElementById("Submit").addEventListener("click" ,async() => {
+  
+  console.log(NFTDescription, NFTName,contract ,(metadata_hash))
+
+
+  const mintNFT = async(tokenURI) => {
+    ethereum
+      .request({
+        method: "eth_sendTransaction",
+        params: [
+          {
+            from: selectedACC,
+            to: contractAddress,
+            nonce: nonce_needed,
+            gas: "500000",
+            data: contract.methods.mintNFT(selectedACC, tokenURI).encodeABI(),
+          },
+        ],
+      })
+      .then((result) => {
+        console.log(result);
+        // The result varies by by RPC method.
+        // For example, this method will return a transaction hash hexadecimal string on success.
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  mintNFT(metadata_hash);
+
+
+  document.querySelector(".nes-textarea").value = ""
+  document.querySelector(".nes-input").value = ""
+
+  
+})
--- js/dist/NFT.dev.js
+++ js/dist/NFT.dev.js
@@ -4,12 +4,24 @@ var web3btn = document.getElementById("web3connect");
 var acc = document.getElementById("acc");
 var test = document.getElementById("test");
 var meme = document.getElementById("meme-image");
+var contractAddress = "0x248dD7b699042C53fAc61A8f4efA15f413E5477b";
+var contract_json;
+fetch("../artifacts/MyNFT.json").then(function (res) {
+  return res.json();
+}).then(function (result) {
+  contract_json = result;
+});
 var Web3Modal = window.Web3Modal["default"];
 var WalletConnectProvider = window.WalletConnectProvider["default"];
 var selectedACC;
+var metadata_hash = null;
+var nonce_needed = null;
+var contract = null;
+var NFTName = null;
+var NFTDescription = null;
 
 var ConnectWallet = function ConnectWallet() {
-  var providerOptions, web3Modal, provider, web3, accounts, chainId;
+  var providerOptions, web3Modal, provider, web3, accounts, chainId, nftContract, nonce;
   return regeneratorRuntime.async(function ConnectWallet$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
@@ -55,6 +67,15 @@ var ConnectWallet = function ConnectWallet() {
           console.log(chainId);
           selectedACC = accounts[0];
           acc.innerText = selectedACC;
+          nftContract = new web3.eth.Contract(contract_json.abi, contractAddress);
+          contract = nftContract;
+          console.log(contract);
+          _context.next = 20;
+          return regeneratorRuntime.awrap(web3.eth.getTransactionCount(selectedACC, "latest"));
+
+        case 20:
+          nonce = _context.sent;
+          nonce_needed = nonce.toString();
 
           if (selectedACC != null | undefined) {
             console.log(selectedACC);
@@ -62,7 +83,7 @@ var ConnectWallet = function ConnectWallet() {
             console.log("yo! connect the damn wallet");
           }
 
-        case 16:
+        case 23:
         case "end":
           return _context.stop();
       }
@@ -70,51 +91,22 @@ var ConnectWallet = function ConnectWallet() {
   });
 };
 
+;
 web3btn.addEventListener("click", function () {
   ConnectWallet();
 });
-
-var getNFT = function getNFT() {};
-
 document.getElementById("nft-file-input").addEventListener("change", function _callee(res) {
-  var pinataApiKey, pinataSecretApiKey, url, data, metadata;
+  var pinataApiKey, pinataSecretApiKey, url, jsonUrl, data;
   return regeneratorRuntime.async(function _callee$(_context2) {
     while (1) {
       switch (_context2.prev = _context2.next) {
         case 0:
-          /* console.log(res.target.files[0]);
-          const ipfs = window.IpfsApi("gateway.ipfs.io"); // Connect to IPFS 
-          const Buffer = window.IpfsApi().Buffer;
-          let buf_blob = await res.target.files[0].arrayBuffer();
-          let buf = Buffer(buf_blob);
-           ipfs.files.add(buf, (err, result) => {
-            // Upload buffer to IPFS
-            if (err) {
-              console.error(err);
-              return;
-            }
-            let url = `https://ipfs.io/ipfs/${result[0].hash}`;
-            console.log(`Url --> ${url}`);
-          });
-           ipfs.add(buf).then((res) => console.log(res)); */
-          pinataApiKey = process.env.pinataApiKey;
-          pinataSecretApiKey = process.env.pinataSecretApiKey;
+          pinataApiKey = "a770d310d147135d5ec4";
+          pinataSecretApiKey = "076b05a1c38c2910d32a8079e1007d52b8c02264990e0af61fa0e544cd760c78";
           url = "https://api.pinata.cloud/pinning/pinFileToIPFS";
+          jsonUrl = "https://api.pinata.cloud/pinning/pinJSONToIPFS";
           data = new FormData();
-          /*   const ipfs = window.IpfsApi("gateway.ipfs.io"); // Connect to IPFS
-            const Buffer = window.IpfsApi().Buffer;
-            let buf_blob = await res.target.files[0].arrayBuffer();
-            let buf = Buffer(buf_blob);
-            console.log(buf) */
-
           data.append("file", res.target.files[0]);
-          metadata = JSON.stringify({
-            name: "testname",
-            keyvalues: {
-              exampleKey: "exampleValue"
-            }
-          });
-          data.append("pinataMetadata", metadata);
           axios.post(url, data, {
             maxBodyLength: "Infinity",
             //this is needed to prevent axios from erroring out with large files
@@ -126,13 +118,91 @@ document.getElementById("nft-file-input").addEventListener("change", function _c
           }).then(function (response) {
             //handle response here
             console.log(response.data);
-            console.log("https://ipfs.io/ipfs/" + response.data.IpfsHash);
-          })["catch"](function (error) {//handle error here
+            console.log("image url = https://ipfs.io/ipfs/" + response.data.IpfsHash);
+            var metadata = JSON.stringify({
+              name: NFTName,
+              Info: {
+                description: NFTDescription,
+                image: "https://ipfs.io/ipfs/" + response.data.IpfsHash
+              }
+            });
+            axios.post(jsonUrl, metadata, {
+              maxBodyLength: "Infinity",
+              //this is needed to prevent axios from erroring out with large files
+              headers: {
+                "Content-Type": 'application/json',
+                pinata_api_key: pinataApiKey,
+                pinata_secret_api_key: pinataSecretApiKey
+              }
+            }).then(function (res) {
+              console.log("metadata url = https://ipfs.io/ipfs/".concat(res.data.IpfsHash));
+              var gg = "https://ipfs.io/ipfs/".concat(res.data.IpfsHash);
+              metadata_hash = gg;
+              console.log("image uploaded");
+            });
+          })["catch"](function (error) {
+            console.log(error);
           });
 
-        case 8:
+        case 7:
         case "end":
           return _context2.stop();
+      }
+    }
+  });
+});
+document.querySelector(".nes-input").addEventListener("change", function (r) {
+  console.log(r.target.value);
+  NFTDescription = r.target.value;
+});
+document.querySelector(".nes-textarea").addEventListener("change", function (res) {
+  console.log(res.target.value);
+  NFTName = res.target.value;
+});
+document.getElementById("Submit").addEventListener("click", function _callee2() {
+  var mintNFT;
+  return regeneratorRuntime.async(function _callee2$(_context4) {
+    while (1) {
+      switch (_context4.prev = _context4.next) {
+        case 0:
+          console.log(NFTDescription, NFTName, contract, metadata_hash);
+
+          mintNFT = function mintNFT(tokenURI) {
+            return regeneratorRuntime.async(function mintNFT$(_context3) {
+              while (1) {
+                switch (_context3.prev = _context3.next) {
+                  case 0:
+                    ethereum.request({
+                      method: "eth_sendTransaction",
+                      params: [{
+                        from: selectedACC,
+                        to: contractAddress,
+                        nonce: nonce_needed,
+                        gas: "500000",
+                        data: contract.methods.mintNFT(selectedACC, tokenURI).encodeABI()
+                      }]
+                    }).then(function (result) {
+                      console.log(result); // The result varies by by RPC method.
+                      // For example, this method will return a transaction hash hexadecimal string on success.
+                    })["catch"](function (error) {
+                      console.log(error);
+                    });
+
+                  case 1:
+                  case "end":
+                    return _context3.stop();
+                }
+              }
+            });
+          };
+
+          mintNFT(metadata_hash);
+          document.querySelector(".nes-textarea").value = "";
+          document.querySelector(".nes-input").value = "";
+
+        case 5:
+        case "end":
+          return _context4.stop();
       }
     }
   });
